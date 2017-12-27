a669e51d75614c2139a4ca27308db108c2a6cda5
Romulo Auccapuclla <rauccapu@pe.ibm.com>
2017-12-27 15:00:21 -0500

    First commit for devops project

---
+++ src/main/java/devops/DevOpsController.java
@@ -0,0 +1,11 @@
+package devops;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping; @RestController
+
+public class DevOpsController {
+    @RequestMapping("/")
+    public String index() {
+        return "Aplicación de laboratorio v1";
+    }
+}
