00e94500a814f8fe0e3da22ba546f2b81fcce040
Romulo Auccapuclla <rauccapu@oc2744082752.ibm.com>
2018-02-06 20:01:20 -0500

    Add integration tests and build maven docker image

--- devops.sh
+++ devops.sh
@@ -2,6 +2,7 @@
 echo "Begin docker CI/CD locally"
 
 docker volume create --name=mavenrepo || exit -1
+docker build -t maven:alpine maven/
 docker-compose -f docker-ci.yml run --rm maven clean test || exit -1
 docker-compose -f docker-ci.yml run --rm maven clean package || exit -1
 docker-compose build || exit -1
