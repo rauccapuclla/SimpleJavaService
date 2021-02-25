28df3449c42d04063aac78071bdc9cc325c25895
Romulo Auccapuclla <rauccapuclla@gmail.com>
2021-02-25 09:10:32 -0500

    Add docker push script

--- docker-push.sh
+++ docker-push.sh
@@ -1,1 +1,14 @@
 #!/usr/bin/env bash
+echo "Usage: ./$0 [docker image] [tag]"
+DOCKER_IMAGE="rauccapuclla/app"
+TAG="latest"
+if [ ! -z $1 ]; then
+    DOCKER_IMAGE=$1
+fi
+
+if [ ! -z $2 ]; then
+    TAG=$2
+fi
+
+docker tag app $DOCKER_IMAGE:$TAG
+docker push $DOCKER_IMAGE:$TAG
